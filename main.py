dc5d71d5f44273726fe150b705c3bfb29d4f543d
Singhx <126919241+singhx-hub@users.noreply.github.com>
2023-05-24 05:36:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import secrets
 from tkinter import *  # for GUI
 from tkinter import filedialog
 import tkinter as tk
@@ -27,13 +26,25 @@ def showimage():
     lb1.image = img
 
 
-def Hide():
+def hide():
     global secret
     msg = txt.get(1.0, END)
     secret = lsb.hide(str(filename), msg)
 
 
-def Show():
+def hide_and_print():
+    hide()
+    print_text = "Data Hide Successfully!"
+    result_label.configure(text=print_text)
+    txt.delete(1.0, END)  # Clear the text in txt
+    lb1.configure(image='', width=0, height=0)  # Reset the image in lb1
+
+
+result_label = Label(root, text="", font="Arial 14", bg="#2C3539", fg="white")
+result_label.place(x=400, y=610)
+
+
+def show():
     clear_msg = lsb.reveal(filename)
     txt.delete(1.0, END)
     txt.insert(END, clear_msg)
@@ -85,8 +96,9 @@ Label(f3, text="Picture, Image, Photo File", bg="#2C3539", fg="yellow").place(x=
 f4 = Frame(root, bd=3, bg="#2C3539", width=490, height=100, relief=SUNKEN)
 f4.place(x=500, y=470)
 
-Button(f4, text="Hide Data", width=10, height=2, bg="yellow", font="arial 14 bold", command=Hide).place(x=20, y=30)
-Button(f4, text="Show Data", width=10, height=2, bg="green", font="arial 14 bold", command=Show).place(x=340, y=30)
+Button(f4, text="Hide Data", width=10, height=2, bg="yellow", font="arial 14 bold", command=hide_and_print).place(x=20,
+                                                                                                                  y=30)
+Button(f4, text="Show Data", width=10, height=2, bg="green", font="arial 14 bold", command=show).place(x=340, y=30)
 Label(f4, text="Picture, Image, Photo File", bg="#2C3539", fg="yellow").place(x=170, y=5)
 
 root.mainloop()
